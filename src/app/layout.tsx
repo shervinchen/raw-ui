51570dc456b440e19af425a6e9ce3a8898fca834
shervinchen <shervinchen@gmail.com>
2023-05-29 15:15:41 +0300

    chore: support for using styled-jsx in next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import StyledJsxRegistry from './registry';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <StyledJsxRegistry>{children}</StyledJsxRegistry>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+'use client';
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <h1>
+      Hello, Next.js!
+      <style jsx>{`
+        h1 {
+          font-size: 40px;
+        }
+      `}</style>
+    </h1>
+  );
 }
