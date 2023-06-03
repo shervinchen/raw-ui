8994cdcfc366ecbf93dfea822411ad6320c96c78
shervinchen <shervinchen@gmail.com>
2023-06-03 20:17:15 +0300

    docs: add basic layout and style of home page

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -6,48 +6,55 @@ import { GitHub, Moon } from 'react-feather';
 
 export default function Navbar() {
   return (
-    <nav className="nav-container">
-      <div className="nav-inner">
+    <header className="header-container">
+      <nav className="nav-wrapper">
         <Link href="/" className="logo-wrapper">
           <Image src="/logo.svg" alt="logo" width={24} height={24} />
-          <span className="logo-name">Raw UI</span>
+          <span className="title">Raw UI</span>
         </Link>
-        <Link href="https://github.com/shervinchen/raw-ui">
-          <GitHub size={24} color="#000" />
-        </Link>
-        <Moon size={24} color="#000" cursor="pointer" />
-      </div>
+        <div className="menu-wrapper">
+          <Link href="https://github.com/shervinchen/raw-ui">
+            <GitHub size={24} color="#000" />
+          </Link>
+          <Moon size={24} color="#000" cursor="pointer" />
+        </div>
+      </nav>
       <style jsx>{`
-        .nav-container {
+        .header-container {
           position: sticky;
           top: 0;
           width: 100%;
           background-color: #fff;
           box-shadow: inset 0 -1px 0 0 #eaeaea;
+          z-index: 1;
         }
-        .nav-inner {
+        .nav-wrapper {
           display: flex;
           align-items: center;
-          gap: 8px;
           max-width: 1440px;
           height: 64px;
           padding: 0 24px;
           margin-left: auto;
           margin-right: auto;
         }
-        .nav-inner :global(a) {
+        .nav-wrapper :global(a) {
           display: flex;
           align-items: center;
         }
-        .nav-inner :global(.logo-wrapper) {
+        .nav-wrapper :global(.logo-wrapper) {
           gap: 10px;
           margin-right: auto;
         }
-        .logo-name {
+        .logo-wrapper .title {
           font-size: 18px;
           font-weight: bold;
         }
+        .menu-wrapper {
+          display: flex;
+          align-items: center;
+          gap: 8px;
+        }
       `}</style>
-    </nav>
+    </header>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,56 @@
 'use client';
 
+import Image from 'next/image';
+import { GitHub } from 'react-feather';
+import { Button } from '@/packages';
+
 export default function Page() {
   return (
-    <h1>
-      Hello, Next.js!
+    <div className="home-container">
+      <main className="hero-wrapper">
+        <Image src="/logo.svg" alt="logo" width={100} height={100} />
+        <h1 className="title">Raw UI</h1>
+        <h2 className="description">
+          A minimalist and customizable React component library for web
+          applications.
+        </h2>
+        <div className="buttons">
+          <Button type="primary">Get Started</Button>
+          <Button icon={<GitHub />}>Github</Button>
+        </div>
+      </main>
       <style jsx>{`
-        h1 {
-          font-size: 40px;
+        .home-container {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          height: calc(100vh - 64px);
+          padding: 0 24px;
+        }
+        .hero-wrapper {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          max-width: 1440px;
+          text-align: center;
+        }
+        .title {
+          font-size: 60px;
+          font-weight: 700;
+          color: #000;
+        }
+        .description {
+          font-size: 24px;
+          font-weight: 500;
+          color: #666;
+        }
+        .buttons {
+          display: flex;
+          gap: 12px;
+          margin-top: 40px;
         }
       `}</style>
-    </h1>
+    </div>
   );
 }
