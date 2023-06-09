32f873157942b7024a9460a12311205d49fbaceb
shervinchen <shervinchen@gmail.com>
2023-06-09 17:47:11 +0300

    chore(docs): optimize markdown styles with @tailwindcss/typography

--- src/app/(article)/layout.tsx
+++ src/app/(article)/layout.tsx
@@ -6,9 +6,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex max-w-8xl mx-auto py-0 px-6">
       <Sidebar />
-      <main className="min-h-[calc(100vh-64px)] px-0 py-4 flex-grow md:px-6">
+      <main className="min-h-[calc(100vh-64px)] px-0 py-4 flex-grow flex-shrink basis-0 md:px-6">
         <div className="breadcrumb"></div>
-        <article>{children}</article>
+        <article className="prose dark:prose-invert max-w-none">
+          {children}
+        </article>
       </main>
     </div>
   );
