b5f80280e23895b256d6e9112111d00247e2832e
shervinchen <shervinchen@gmail.com>
2023-06-11 11:53:35 +0300

    refactor(Button): separate components of the button demo

--- demo/App.tsx
+++ demo/App.tsx
@@ -4,8 +4,6 @@ import {
   Button,
   Loading,
   Input,
-  Checkbox,
-  CheckboxGroupValue,
   Radio,
   RadioValue,
   Toggle,
@@ -24,6 +22,16 @@ import {
   DemoCheckboxIndeterminate,
   DemoCheckboxWithLabel,
 } from './checkbox';
+import {
+  DemoButtonDisabled,
+  DemoButtonIcon,
+  DemoButtonLoading,
+  DemoButtonSizes,
+  DemoButtonTypes,
+  DemoButtonVariantGhost,
+  DemoButtonVariantOutline,
+  DemoButtonVariantShadow,
+} from './button';
 
 const Container: FC<PropsWithChildren<{ title: string }>> = ({
   title,
@@ -86,111 +94,24 @@ function App() {
     <div>
       <Container title="Button">
         <Wrapper title="Types">
-          <Unit layout="row">
-            <Button>Default</Button>
-            <Button type="primary">Primary</Button>
-            <Button type="success">Success</Button>
-            <Button type="warning">Warning</Button>
-            <Button type="error">Error</Button>
-          </Unit>
+          <DemoButtonTypes />
         </Wrapper>
         <Wrapper title="Sizes">
-          <Unit layout="row">
-            <Button size="sm">Small</Button>
-            <Button>Medium</Button>
-            <Button size="lg">Large</Button>
-          </Unit>
+          <DemoButtonSizes />
         </Wrapper>
         <Wrapper title="Variant">
-          <Unit title="Outline" layout="row">
-            <Button type="primary" variant="outline">
-              Primary
-            </Button>
-            <Button type="success" variant="outline">
-              Success
-            </Button>
-            <Button type="warning" variant="outline">
-              Warning
-            </Button>
-            <Button type="error" variant="outline">
-              Error
-            </Button>
-          </Unit>
-          <Unit title="Ghost" layout="row">
-            <Button variant="ghost">Default</Button>
-            <Button type="primary" variant="ghost">
-              Primary
-            </Button>
-            <Button type="success" variant="ghost">
-              Success
-            </Button>
-            <Button type="warning" variant="ghost">
-              Warning
-            </Button>
-            <Button type="error" variant="ghost">
-              Error
-            </Button>
-          </Unit>
-          <Unit title="Shadow" layout="row">
-            <Button variant="shadow">Default</Button>
-            <Button type="primary" variant="shadow">
-              Primary
-            </Button>
-            <Button type="success" variant="shadow">
-              Success
-            </Button>
-            <Button type="warning" variant="shadow">
-              Warning
-            </Button>
-            <Button type="error" variant="shadow">
-              Error
-            </Button>
-          </Unit>
+          <DemoButtonVariantOutline title="Outline" />
+          <DemoButtonVariantGhost title="Ghost" />
+          <DemoButtonVariantShadow title="Shadow" />
         </Wrapper>
         <Wrapper title="Loading">
-          <Unit layout="row">
-            <Button loading>Default</Button>
-            <Button type="primary" loading>
-              Primary
-            </Button>
-            <Button type="success" variant="outline" loading>
-              Success
-            </Button>
-            <Button type="warning" variant="ghost" loading>
-              Warning
-            </Button>
-            <Button type="error" variant="shadow" loading>
-              Error
-            </Button>
-          </Unit>
+          <DemoButtonLoading />
         </Wrapper>
         <Wrapper title="Disabled">
-          <Unit layout="row">
-            <Button disabled>Default</Button>
-            <Button type="primary" disabled>
-              Primary
-            </Button>
-            <Button type="primary" variant="outline" disabled>
-              Outline
-            </Button>
-            <Button type="primary" variant="ghost" disabled>
-              Ghost
-            </Button>
-            <Button type="primary" variant="shadow" disabled>
-              Shadow
-            </Button>
-          </Unit>
+          <DemoButtonDisabled />
         </Wrapper>
         <Wrapper title="Icons">
-          <Unit layout="row">
-            <Button icon={<Search />} />
-            <Button icon={<Search />}>Search</Button>
-            <Button iconRight={<Search />}>Search</Button>
-            <Button type="primary" icon={<Search />} />
-            <Button type="primary" icon={<Search />}>
-              Search
-            </Button>
-          </Unit>
+          <DemoButtonIcon />
         </Wrapper>
       </Container>
       <Container title="Loading">
---
+++ demo/button/index.tsx
@@ -0,0 +1,139 @@
+import React from 'react';
+import { Search } from 'react-feather';
+import Unit from '../Unit';
+import { Button } from '@/packages';
+
+export function DemoButtonTypes() {
+  return (
+    <Unit layout="row">
+      <Button>Default</Button>
+      <Button type="primary">Primary</Button>
+      <Button type="success">Success</Button>
+      <Button type="warning">Warning</Button>
+      <Button type="error">Error</Button>
+    </Unit>
+  );
+}
+
+export function DemoButtonSizes() {
+  return (
+    <Unit layout="row">
+      <Button size="sm">Small</Button>
+      <Button>Medium</Button>
+      <Button size="lg">Large</Button>
+    </Unit>
+  );
+}
+
+export function DemoButtonVariantOutline({ title }: { title: string }) {
+  return (
+    <Unit title={title} layout="row">
+      <Button type="primary" variant="outline">
+        Primary
+      </Button>
+      <Button type="success" variant="outline">
+        Success
+      </Button>
+      <Button type="warning" variant="outline">
+        Warning
+      </Button>
+      <Button type="error" variant="outline">
+        Error
+      </Button>
+    </Unit>
+  );
+}
+
+export function DemoButtonVariantGhost({ title }: { title: string }) {
+  return (
+    <Unit title={title} layout="row">
+      <Button variant="ghost">Default</Button>
+      <Button type="primary" variant="ghost">
+        Primary
+      </Button>
+      <Button type="success" variant="ghost">
+        Success
+      </Button>
+      <Button type="warning" variant="ghost">
+        Warning
+      </Button>
+      <Button type="error" variant="ghost">
+        Error
+      </Button>
+    </Unit>
+  );
+}
+
+export function DemoButtonVariantShadow({ title }: { title: string }) {
+  return (
+    <Unit title={title} layout="row">
+      <Button variant="shadow">Default</Button>
+      <Button type="primary" variant="shadow">
+        Primary
+      </Button>
+      <Button type="success" variant="shadow">
+        Success
+      </Button>
+      <Button type="warning" variant="shadow">
+        Warning
+      </Button>
+      <Button type="error" variant="shadow">
+        Error
+      </Button>
+    </Unit>
+  );
+}
+
+export function DemoButtonLoading() {
+  return (
+    <Unit layout="row">
+      <Button loading>Default</Button>
+      <Button type="primary" loading>
+        Primary
+      </Button>
+      <Button type="success" variant="outline" loading>
+        Success
+      </Button>
+      <Button type="warning" variant="ghost" loading>
+        Warning
+      </Button>
+      <Button type="error" variant="shadow" loading>
+        Error
+      </Button>
+    </Unit>
+  );
+}
+
+export function DemoButtonDisabled() {
+  return (
+    <Unit layout="row">
+      <Button disabled>Default</Button>
+      <Button type="primary" disabled>
+        Primary
+      </Button>
+      <Button type="primary" variant="outline" disabled>
+        Outline
+      </Button>
+      <Button type="primary" variant="ghost" disabled>
+        Ghost
+      </Button>
+      <Button type="primary" variant="shadow" disabled>
+        Shadow
+      </Button>
+    </Unit>
+  );
+}
+
+export function DemoButtonIcon() {
+  return (
+    <Unit layout="row">
+      <Button icon={<Search />} />
+      <Button icon={<Search />}>Search</Button>
+      <Button iconRight={<Search />}>Search</Button>
+      <Button type="primary" icon={<Search />} />
+      <Button type="primary" icon={<Search />}>
+        Search
+      </Button>
+    </Unit>
+  );
+}
